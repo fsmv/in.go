709386f7ba768baa069ee384c3288b9d473a96dc
Andrew Kallmeyer <ask@ask.systems>
2021-08-25 20:10:31 -0700

    Made some of the constants into flags

--- in.go
+++ in.go
@@ -11,24 +11,30 @@ package main
 import (
   "os"
   "fmt"
+  "flag"
   "time"
   "strings"
 )
 
+var (
+  BarLen = flag.Int("length", 40,
+    "The length of the progress bar (including the end chars) in characters")
+  Step = flag.Duration("step", time.Second,
+    "The amount of time between display updates")
+)
+
 const (
-  BarLen = 40
   BarFilled = '='
   BarEmpty = '-'
-  Step = time.Second
 )
 
 const ClearLine = "\033[K" // ESC[K, the ECMA-48 CSI code for Erase line. See man 4 console_codes
 
 func progressbar(elapsed, total int64) string {
-  const realBarLen = BarLen-2 // to account for the [ and ]
+  realBarLen := *BarLen-2 // to account for the [ and ]
 
   // Solve for progress: total / BarLen = elapsed / progress
-  progress := int(elapsed / (total / realBarLen))
+  progress := int(elapsed / (total / int64(realBarLen)))
   if progress < 0 || progress > realBarLen {
     fmt.Fprintln(os.Stderr, "Bad miscaculation:", progress, elapsed, total, realBarLen)
     os.Exit(2)
@@ -47,20 +53,28 @@ func progressbar(elapsed, total int64) string {
 }
 
 func main() {
-  if len(os.Args) < 2 {
-    fmt.Fprintf(os.Stderr, "Usage example: %v 2m30s\n", os.Args[0])
+  flag.Usage = func() {
+    fmt.Fprintf(os.Stderr, "Usage: %v [flags] [duration]\nEx: %v 2m30s && run_a_command\n\n",
+      os.Args[0], os.Args[0])
+    fmt.Fprintf(os.Stderr, "Flags:\n")
+    flag.PrintDefaults()
+  }
+  flag.Parse()
+  args := flag.Args()
+  if len(args) == 0 {
+    flag.Usage()
     os.Exit(127)
   }
-  sleeptime, err := time.ParseDuration(os.Args[1])
+  sleeptime, err := time.ParseDuration(args[0])
   if err != nil {
-    fmt.Fprintln(os.Stderr, "Invalid duration:", os.Args[1])
+    fmt.Fprintln(os.Stderr, "Invalid duration:", args[0])
     os.Exit(1)
   }
-  for elapsed := 0*time.Second; elapsed < sleeptime; elapsed += Step {
+  for elapsed := 0*time.Second; elapsed < sleeptime; elapsed += *Step {
     fmt.Fprintf(os.Stderr, "\r%s %v/%v%s",
       progressbar(elapsed.Milliseconds(), sleeptime.Milliseconds()),
       elapsed, sleeptime, ClearLine)
-    time.Sleep(Step)
+    time.Sleep(*Step)
   }
   fmt.Fprintf(os.Stderr, "\rDing!%s\n", ClearLine)
 }
