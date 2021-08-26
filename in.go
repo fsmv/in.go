9a3e8f78d274e0aeb5991a6325ad5cd833d414df
Andrew Kallmeyer <ask@ask.systems>
2021-08-25 22:53:40 -0700

    Fix the edge cases so users will never see an error message
    Fixed:
     - If -length was < 3 before we would panic with divide by zero
     - If the total wait time was less than realBarLen ms we would panic
       with divide by zero
    
    Now -length 0 is a feature where we only display the '1s/10s' part. Note
    that -step of <= 0 works fine, time.Sleep allows it, we'll just update
    as fast as possible. Also negative wait times get a flag parser error.
    
    I don't think it's possible to ever print "Bad miscalculation" without
    editing the code now (0 wait time skips the for loop), but I'll leave it
    in for future editors.

--- in.go
+++ in.go
@@ -32,16 +32,16 @@ const ClearLine = "\033[K" // ESC[K, the ECMA-48 CSI code for Erase line. See ma
 
 var bar bytes.Buffer
 
-func progressbar(elapsed, total int64) []byte {
+func progressbar(elapsed, total float64) []byte {
   realBarLen := *BarLen-2 // to account for the [ and ]
-
-  // Solve for progress: total / BarLen = elapsed / progress
-  progress := int(elapsed / (total / int64(realBarLen)))
+  if realBarLen < 1 {
+    return nil // The user asked for no progress bar
+  }
+  progress := int((elapsed / total) * float64(realBarLen))
   if progress < 0 || progress > realBarLen {
     fmt.Fprintln(os.Stderr, "Bad miscaculation:", progress, elapsed, total, realBarLen)
     os.Exit(2)
   }
-
   bar.Reset()
   bar.WriteRune('[')
   for i := 0; i < progress; i+=1 {
@@ -51,6 +51,7 @@ func progressbar(elapsed, total int64) []byte {
     bar.WriteRune(BarEmpty)
   }
   bar.WriteRune(']')
+  bar.WriteRune(' ') // the space is here so we don't print it when realBarLen < 1
   return bar.Bytes()
 }
 
@@ -74,8 +75,8 @@ func main() {
   }
   start := time.Now()
   for elapsed := 0*time.Second; elapsed < sleeptime; elapsed = time.Since(start) {
-    fmt.Fprintf(os.Stderr, "\r%s %v/%v%s",
-      progressbar(elapsed.Milliseconds(), sleeptime.Milliseconds()),
+    fmt.Fprintf(os.Stderr, "\r%s%v/%v%s",
+      progressbar(elapsed.Seconds(), sleeptime.Seconds()),
       elapsed.Truncate(*Step), sleeptime, ClearLine)
     if remaining := sleeptime - elapsed; remaining < *Step {
       time.Sleep(remaining)
