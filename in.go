a1d1d56a8ea003efb93a56d35e9bc671314dbe41
Andrew Kallmeyer <ask@ask.systems>
2021-08-29 17:03:00 -0700

    Clean up variable names and don't print seconds when there are none left Before this we were printing 1m0s for 1m waits, now we skip the 0s

--- in.go
+++ in.go
@@ -19,44 +19,44 @@ var (
   Step = flag.Duration("step", time.Second, "The amount of time between display updates")
   BarStyle = flag.String("style", "[=-]", "The specification for the progress bar style.\nMust be exactly 4 chars {start, filled, unfilled, end}.")
   BarLen = flag.Int("length", 40, "The length of the progress bar (including the end chars) in characters")
-  durationBuffer = make([]byte, 32) // max string is "2540400h59m59.000000001"
 )
 
+var durationBuffer = make([]byte, 32) // max string is "2540400h59m59.000000001"
 const ClearLine = "\033[K" // ESC[K, the ECMA-48 CSI code for Erase line. See man 4 console_codes
 
-func writeProgressbar(bar *bytes.Buffer, elapsed, total float64) {
-  realBarLen := *BarLen-2 // to account for the beginning and end chars
-  if realBarLen < 1 {
-    return // The user asked for no progress bar
+func writeProgressbar(out *bytes.Buffer, elapsed, total float64) {
+  if realBarLen := *BarLen-2; realBarLen > 0 { // handle the start and end chars
+    progress := int((elapsed / total) * float64(realBarLen))
+    out.WriteByte((*BarStyle)[0])
+    for i := 0; i < progress; i+=1 {
+      out.WriteByte((*BarStyle)[1])
+    }
+    for i := 0; i < (realBarLen - progress); i+=1 {
+      out.WriteByte((*BarStyle)[2])
+    }
+    out.Write([]byte{(*BarStyle)[3],' '}) // the space is here so we don't print it when realBarLen < 1
   }
-  progress := int((elapsed / total) * float64(realBarLen))
-  bar.WriteByte((*BarStyle)[0])
-  for i := 0; i < progress; i+=1 {
-    bar.WriteByte((*BarStyle)[1])
-  }
-  for i := 0; i < (realBarLen - progress); i+=1 {
-    bar.WriteByte((*BarStyle)[2])
-  }
-  bar.Write([]byte{(*BarStyle)[3],' '}) // the space is here so we don't print it when realBarLen < 1
 }
 
-func writeTimeUnit(out *bytes.Buffer, val *int64, unit time.Duration, label byte) {
-  if u := *val / unit.Nanoseconds(); u >= 1 {
+func writeTimeUnit(out *bytes.Buffer, remainingNanos *int64, unit time.Duration, label byte) {
+  if u := *remainingNanos / unit.Nanoseconds(); u >= 1 {
     durationBuffer = strconv.AppendInt(durationBuffer, u, 10)
     durationBuffer = append(durationBuffer, label)
-    *val -= u * unit.Nanoseconds()
+    *remainingNanos -= u * unit.Nanoseconds()
   }
 }
 
 // time.Duration.String() allocates the result on the heap, so I reimplemented to avoid allocation
 func writeDuration(out *bytes.Buffer, d time.Duration) {
   durationBuffer = durationBuffer[:0]
-  val := d.Nanoseconds()
-  writeTimeUnit(out, &val, time.Hour, 'h')
-  writeTimeUnit(out, &val, time.Minute, 'm')
-  durationBuffer = strconv.AppendFloat(durationBuffer, float64(val)/1e9, 'f', -1, 64)
+  remainingNanos := d.Nanoseconds()
+  writeTimeUnit(out, &remainingNanos, time.Hour, 'h')
+  writeTimeUnit(out, &remainingNanos, time.Minute, 'm')
+  if len(durationBuffer) == 0 || remainingNanos != 0 {
+    durationBuffer = strconv.AppendFloat(durationBuffer, float64(remainingNanos)/1e9, 'f', -1, 64)
+    durationBuffer = append(durationBuffer, 's')
+  }
   out.Write(durationBuffer)
-  out.WriteRune('s')
 }
 
 func main() {
@@ -65,10 +65,6 @@ func main() {
     flag.PrintDefaults()
   }
   flag.Parse()
-  if len(*BarStyle) != 4 {
-    fmt.Fprintln(os.Stderr, "Invalid style argument, must be exactly 4 characters")
-    os.Exit(1)
-  }
   args := flag.Args()
   if len(args) == 0 {
     flag.Usage()
@@ -77,6 +73,10 @@ func main() {
   sleeptime, err := time.ParseDuration(args[0])
   if err != nil {
     fmt.Fprintln(os.Stderr, "Invalid duration:", args[0])
+    os.Exit(1)
+  }
+  if len(*BarStyle) != 4 {
+    fmt.Fprintln(os.Stderr, "Invalid style argument, must be exactly 4 characters")
     os.Exit(1)
   }
   start := time.Now()
